cbeac116bc17163ab50b62fc11763406d3b19883
@maksim.mushko <PolliCantelli.1924>
2023-03-30 14:37:44 +0200

    Create simple Sprint Boot server

---
+++ src/main/java/com/dcsTest/test/MyRestController.java
@@ -0,0 +1,12 @@
+package com.dcsTest.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
